bbccf85c88e5c9a98c16d105245acd6dd94f6867
Fysalidev <fysalidev@macbookairglg.home>
2022-11-10 17:58:41 +0100

    Feature(Form) : Ok | Ajuster Reset | Ajuster Css

--- src/components/form/FormEmployees.jsx
+++ src/components/form/FormEmployees.jsx
@@ -8,6 +8,8 @@ const validationSchema = Yup.object().shape({
   lastName: Yup.string().required("Ce champ est obligatoire"),
   birthDate: Yup.string().required("Ce champ est obligatoire"),
   startDate: Yup.string().required("Ce champ est obligatoire"),
+  street: Yup.string().required("Ce champ est obligatoire"),
+  city: Yup.string().required("Ce champ est obligatoire"),
 });
 
 const initialValues = {
@@ -15,6 +17,8 @@ const initialValues = {
   lastName: "",
   birthDate: "",
   startDate: "",
+  street: "",
+  city: "",
 };
 
 const handleSubmit = (values) => {
@@ -33,12 +37,42 @@ function FormEmployees() {
           <FormTitle>Create Employee</FormTitle>
           <Entries>
             <StyledLegend>Employee</StyledLegend>
+            <Entrie>
+              <StyledLabel htmlFor="firstName">First Name</StyledLabel>
+              <StyledField type="text" id="firstName" name="firstName" />
+              <ErrorMessage name="firstName" />
+            </Entrie>
+            <Entrie>
+              <StyledLabel htmlFor="lastName">Last Name</StyledLabel>
+              <StyledField type="text" id="lastName" name="lastName" />
+              <ErrorMessage name="lastName" />
+            </Entrie>
+            <Entrie>
+              <StyledLabel htmlFor="birthDate">Date of Birth</StyledLabel>
+              <StyledField type="text" id="birthDate" name="birthDate" />
+              <ErrorMessage name="birthDate" />
+            </Entrie>
+            <Entrie>
+              <StyledLabel htmlFor="startDate">Start Date</StyledLabel>
+              <StyledField type="text" id="startDate" name="startDate" />
+              <ErrorMessage name="startDate" />
+            </Entrie>
           </Entries>
           <Entries>
             <StyledLegend>Address</StyledLegend>
+            <Entrie>
+              <StyledLabel htmlFor="street">Street :</StyledLabel>
+              <StyledField type="text" id="street" name="street" />
+              <ErrorMessage name="startDate" />
+            </Entrie>
+            <Entrie>
+              <StyledLabel htmlFor="city">City</StyledLabel>
+              <StyledField type="text" id="city" name="city" />
+              <ErrorMessage name="city" />
+            </Entrie>
           </Entries>
           <FormButton type="submit">Save</FormButton>
-          <FormButton onclick={resetForm}>Cancel</FormButton>
+          <FormButton type="button" onclick={()=> {resetForm}}>Cancel</FormButton>
         </StyledForm>
       )}
     </Formik>
@@ -62,20 +96,60 @@ const StyledForm = styled(Form)`
 const FormTitle = styled.h2`
   color: white;
   font-family: "Comfortaa", cursive;
+  font-weight: bold;
   text-align: center;
 `;
 
 const Entries = styled.fieldset`
   border-radius: 0.4rem;
-  margin: 1rem;
-  padding: 1rem;
-  color: white;
+  /* margin: 1rem; */
+  /* padding: 1rem; */
+  color: purple;
   font-family: "Comfortaa", cursive;
+  display: flex;
+  flex-direction: column;
+  gap:0.5rem;
 `;
 
 const StyledLegend = styled.legend`
   text-align: start;
   padding: 0 0.5rem 0 0.5rem;
+  font-weight: bold;
+  font-size: 1.1rem;
+  color: white;
+`;
+
+const Entrie = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.5rem;
+  color: white;
+`;
+
+const StyledLabel = styled.label`
+  font-weight: bold;
+  font-size: 1rem;
+`;
+
+const StyledField = styled(Field)`
+  width: 100%;
+  height: 2rem;
+  border-radius: 5rem;
+  border: solid 2px white;
+  background: transparent;
+  font-family: "Comfortaa", cursive;
+  font-size: 1rem;
+  color: orange;
+  font-weight: bold;
+  text-align: center;
+
+  &&:focus {
+    outline: none;
+    border: solid 2px purple;
+    background: white;
+    opacity: 0.9;
+  }
 `;
 
 const FormButton = styled.button`
