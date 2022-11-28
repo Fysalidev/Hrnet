07b7a4613b4cae37f5ee19f90875fcea3fffb996
ROBILLIARD Grégory <gregory.robilliard@icloud.com>
2022-11-28 10:59:26 +0100

    Form(Validation) : adjustments

--- src/components/form/utils/validation.js
+++ src/components/form/utils/validation.js
@@ -1,23 +1,31 @@
 import * as Yup from "yup";
 
 export const validationSchema = Yup.object().shape({
-  firstName: Yup.string().required("First name required 😜"),
-  lastName: Yup.string().required("Last name required 😜"),
+  firstName: Yup.string()
+    .min(2, "First name is to short 😜")
+    .max(50, "First name is to long 😜")
+    .required("First name required 😜"),
+  lastName: Yup.string()
+    .min(2, "First name is to short 😜")
+    .max(50, "First name is to long 😜")
+    .required("Last name required 😜"),
   birthDate: Yup.string()
+    .required("Date of birth required 😜")
     .matches(
       /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/,
       "DD/MM/YYYY required 😜"
-    )
-    .required("Date of birth required"),
+    ),
   startDate: Yup.string()
+    .required("Start Date required 😜")
     .matches(
       /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/,
       "DD/MM/YYYY required 😜"
-    )
-    .required("Start Date required 😜"),
+    ),
   street: Yup.string().required("Street required 😜"),
   city: Yup.string().required("City required 😜"),
   state: Yup.string().min(1, "Select a state").required("State required 😜"),
   zipCode: Yup.string().required("Zip Code required 😜"),
-  department: Yup.string().min(1, "Select a department").required("Department required 😜"),
+  department: Yup.string()
+    .min(1, "Select a department 😜")
+    .required("Department required 😜"),
 });
